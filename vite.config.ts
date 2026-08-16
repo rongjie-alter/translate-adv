1205563f15d100866e776093b53f7e7b55ebfa12
Rong Jie <71772241+rongjie-alter@users.noreply.github.com>
2026-08-16 09:23:07 +0800

    chore: vite port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import preact from "@preact/preset-vite";
 
 export default defineConfig({
   plugins: [preact()],
+  server: {
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,
+    strictPort: !!process.env.PORT,
+  },
   test: {
     environment: "happy-dom",
     include: ["src/**/*.test.ts"],
